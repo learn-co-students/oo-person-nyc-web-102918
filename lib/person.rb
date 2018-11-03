2a76812765c3fa7daf647102dda3d25410af81e2
liuh <hanke.liu@gmail.com>
2018-11-03 17:38:16 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = happiness
+
+  end
+
+  def happiness
+    if @happiness < 0
+      @happiness = 0
+    elsif @happiness > 10
+      @happiness = 10
+    else
+      @happiness
+    end
+
+  end
+
+end
